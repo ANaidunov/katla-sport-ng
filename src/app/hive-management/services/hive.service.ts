alexander-rykov/katla-sport

Initial commit.

KatlaSportNg/src/app/hive-management/services/hive.service.ts
@@ -1,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+import { Hive } from '../models/hive';
+import { HiveListItem } from '../models/hive-list-item';
+import { HiveSectionListItem } from '../models/hive-section-list-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HiveService {
+  private url = environment.apiUrl + 'api/hives/';
+
+  constructor(private http: HttpClient) { }
+
+  getHives(): Observable<Array<HiveListItem>> {
+    return this.http.get<Array<HiveListItem>>(this.url);
+  }
+
+  getHive(hiveId: number): Observable<Hive> {
+    return this.http.get<Hive>(`${this.url}${hiveId}`);
+  }
+
+  getHiveSections(hiveId: number): Observable<Array<HiveSectionListItem>> {
+    return this.http.get<Array<HiveSectionListItem>>(`${this.url}${hiveId}/sections`);
+  }
+
+  addHive(hive: Hive): Observable<Hive> {
+    return null;
+  }
+
+  updateHive(hive: Hive): Observable<Hive> {
+    return null
+  }
+
+  deleteHive(hiveId: number): Observable<Object> {
+    return null;
+  }
+
+  setHiveStatus(hiveId: number, deletedStatus: boolean): Observable<Object> {
+    return null;
+  }
+}

ANaidunov/katla-sport-ng

src/app/hive-management/services/hive.service.ts
@@ -1,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+import { Hive } from '../models/hive';
+import { HiveListItem } from '../models/hive-list-item';
+import { HiveSectionListItem } from '../models/hive-section-list-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HiveService {
+  private url = environment.apiUrl + 'api/hives/';
+
+  constructor(private http: HttpClient) { }
+
+  getHives(): Observable<Array<HiveListItem>> {
+    return this.http.get<Array<HiveListItem>>(this.url);
+  }
+
+  getHive(hiveId: number): Observable<Hive> {
+    return this.http.get<Hive>(`${this.url}${hiveId}`);
+  }
+
+  getHiveSections(hiveId: number): Observable<Array<HiveSectionListItem>> {
+    return null;
+  }
+
+  addHive(hive: Hive): Observable<Hive> {
+    return null;
+  }
+
+  updateHive(hive: Hive): Observable<Object> {
+    return null;
+  }
+
+  deleteHive(hiveId: number): Observable<Object> {
+    return null;
+  }
+
+  setHiveStatus(hiveId: number, deletedStatus: boolean): Observable<Object> {
+    return null;
+  }
+}